f334d3d38938020553b807aa6731ea89185cb7fb
Tomasz <tomasz.najda@indoorway.com>
2019-05-03 20:53:57 +0200

    [query] fix: wrong name of model generic type

--- base/src/main/kotlin/com/tomasznajda/rxdao/query/Query.kt
+++ base/src/main/kotlin/com/tomasznajda/rxdao/query/Query.kt
@@ -2,7 +2,7 @@ package com.tomasznajda.rxdao.query
 
 import io.reactivex.Observable
 
-interface Query<ExecutorT, EntityT> {
+interface Query<ExecutorT, ModelT> {
 
-    fun execute(executor: ExecutorT): Observable<List<EntityT>>
+    fun execute(executor: ExecutorT): Observable<List<ModelT>>
 }
--- dbflow/src/main/kotlin/com/tomasznajda/rxdao/dbflow/DbFlowQuery.kt
+++ dbflow/src/main/kotlin/com/tomasznajda/rxdao/dbflow/DbFlowQuery.kt
@@ -3,4 +3,4 @@ package com.tomasznajda.rxdao.dbflow
 import com.raizlabs.android.dbflow.config.DatabaseDefinition
 import com.tomasznajda.rxdao.query.Query
 
-interface DbFlowQuery<EntityT> : Query<DatabaseDefinition, EntityT>
+interface DbFlowQuery<ModelT> : Query<DatabaseDefinition, ModelT>
